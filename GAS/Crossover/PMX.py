e9e04404d59e0604401f56b84fb76f0b31e0c3ed
Jiwon-Baek <baekjiwon@snu.ac.kr>
2024-05-17 14:56:01 +0900

    add skeleton code

---
+++ GAS/Crossover/PMX.py
@@ -0,0 +1,12 @@
+class PMXCrossover:
+    def __init__(self, pc: float):
+        super(PMXCrossover, self).__init__(pc=pc)
+        self.p_mio = 0.9
+        self.num_called = 0
+
+    def cross(self):
+        '''
+        Input : 2 Individual class instance
+        Output : 2 Individual class instance
+        '''
+        pass
--- GAS/GA.py
+++ GAS/GA.py
@@ -1,14 +1,26 @@
 # TODO
-class GAEngine():
-    def __init__(self, crossover = None, mutation = None, selection = None):
+class GAEngine:
+    def __init__(self, cfg, op_data, crossover=None, mutation=None, selection=None):
+        self.cfg = cfg
+        self.op_data = op_data
         self.crossover = crossover
         self.mutation = mutation
         self.selection = selection
 
     def evolve(self):
+        pass
+
 
 if "__main__" == __name__:
-    ga1 = GAEngine(crossover = PMX)
-    ga2 = GAEngine(crossover = OX)
-    ga3 = GAEngine(crossover = PMX)
-    ga4 = GAEngine(crossover = OX)
+    from GAS.Crossover.PMX import PMXCrossover
+    from GAS.Crossover.OX import OXCrossover
+    from Config.Run_Config import Run_Config
+    from Data.Dataset.Dataset import Dataset
+
+    dataset = Dataset('test_1515.txt')
+    cfg = Run_Config()
+
+    ga1 = GAEngine(cfg, dataset.op_data, crossover="PMX", selection='Roulette')
+    ga2 = GAEngine(cfg, dataset.op_data, crossover="OX", selection='Seed')
+    ga3 = GAEngine(cfg, dataset.op_data, crossover="PMX", selection='Tournament')
+    ga4 = GAEngine(cfg, dataset.op_data, crossover="OX", selection='Roulette')
